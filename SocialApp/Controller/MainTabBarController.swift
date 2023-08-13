4bcb0fb4768a4d4a967ed8ea5374840a15d3ffa3
Myungkeon <lanoon28@gmail.com>
2023-08-13 19:33:15 +0900

    Initial Commit

---
+++ SocialApp/Controller/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  SocialApp
+//
+//  Created by Lanoon on 2023/08/13.
+//
+
+import UIKit
+
+class MainTabBarController : UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Lanoon on 2023/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
